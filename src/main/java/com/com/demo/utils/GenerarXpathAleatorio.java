4cf961b4485b47c3dda2e202ad3fc7a6e390466f
regresiontestQA <svc_sfsc_auto_test@rimac.com.pe>
2024-08-20 22:54:11 -0500

    Mantenimiento de la prueba

--- src/main/java/com/com/demo/utils/GenerarXpathAleatorio.java
+++ src/main/java/com/com/demo/utils/GenerarXpathAleatorio.java
@@ -5,8 +5,8 @@ import java.util.concurrent.ThreadLocalRandom;
 public class GenerarXpathAleatorio {
 
     public static Integer generarXPathProductoAleatorio() {
-        int minIndex = 1; // El índice mínimo de los productos
-        int maxIndex = 6; // El índice máximo de los productos
+        int minIndex = 3; // El índice mínimo de los productos
+        int maxIndex = 4; // El índice máximo de los productos
 
         // Generar un número aleatorio entre minIndex y maxIndex
         return ThreadLocalRandom.current().nextInt(minIndex, maxIndex + 1);
